360c311ea175cb8f20c5fc611fbc4c4bb676dcdb
momal <momal.mahar@gmail.com>
2024-07-10 14:57:42 +0200

    inital commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Run home, Jack! Run HOME!</h1>
+        </div>
+    );
+};
+
+export default Home;
